71e98c32b331217e5c8d29b77a77ad1b32705537
aswinsreeram01 <69089254+aswinsreeram01@users.noreply.github.com>
2026-05-17 00:12:34 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // Configuration
 const CONFIG = {
     // Replace this with your Google Apps Script Web App URL
-    API_URL: 'https://script.google.com/macros/library/d/10BKxCLeGeCfNpkvFqtLGkY0d4tSMQMAaBmSTSOMc_KMEih8OsK5exlVo/5'
+    API_URL: 'https://script.google.com/macros/library/d/10BKxCLeGeCfNpkvFqtLGkY0d4tSMQMAaBmSTSOMc_KMEih8OsK5exlVo/6'
 };
